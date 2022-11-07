305a7871e573ff0d8ce07ad73a5c830a0d1abd5c
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-11-07 23:22:17 +0300

    add relations

---
+++ app/models/recipe.rb
@@ -0,0 +1,7 @@
+class Recipe < ApplicationRecord
+    validates :name, presence: true, length: { maximum: 50 }
+
+    has_many :recipe_foods
+    belongs_to :user
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_07_194725) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_07_201425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_194725) do
     t.string "public"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
