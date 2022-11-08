f2de11c6946a724a769ec19583c75316bb34514a
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-11-08 19:22:33 +0300

    Add changes to recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,29 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+
+  def def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user_id = current_user.id
+    if @recipe.save
+     redirect_to recipes_path
+    else
+      render 'new'
+    end
+  end
+  
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
