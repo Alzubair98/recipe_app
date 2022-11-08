8f398f491d7d1497588240d756a68a4d732d915c
AbrahaKahsay <abrahak81@gmail.com>
2022-11-08 23:04:08 +0300

    Fix form in recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :foods
-  resources :recipes
+  get 'recipes/new', to: 'recipes#new' 
+  post 'recipes/new', to: 'recipes#create' 
+  resources :recipes, except: [:new]
 
   # Defines the root path route ("/")
   # root "articles#index"
