fcdad6ac3e95b4e69815e58fc2cadda413c48170
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-11-09 23:16:29 +0300

    add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "pages#home"
   resources :foods
   get 'recipes/new', to: 'recipes#new' 
   post 'recipes/new', to: 'recipes#create' 
