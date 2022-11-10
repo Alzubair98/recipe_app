6d733836e57630aba5d619e9e9e3d02ebf9f4745
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-11-10 19:56:48 +0300

    fix linters add calcuation

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_09_202635) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_10_162307) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.string "measurement_unit"
-    t.string "price"
+    t.decimal "price", precision: 9, scale: 2
     t.string "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -52,10 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_09_202635) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
