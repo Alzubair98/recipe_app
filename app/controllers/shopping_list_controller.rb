121e5a8091161456633514c5fde51d01fb49e5a7
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-11-11 14:26:15 +0300

    generate shopping list

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,14 +1,7 @@
 class ShoppingListController < ApplicationController
 
     def shopping_list 
-        @food = current_user.foods.select('foods.id, foods.name,foods.price,
-            foods.measurement_unit, sum(recipe_foods.quantity) as quantity_sum').joins(:recipe_foods).group('foods.id')
-
-        @value = 0 
-        @items = 0 
-        @food.each do |f| 
-            @value += f.price * f.quantity_sum 
-            @items += 1
-        end
+        @recipes = RecipeFood.all
+        @user = current_user
     end
 end
